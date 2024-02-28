5316e3c370c89d182e271df4f8f999191b8ff649
Vijay <hello.vijayj@gmail.com>
2024-02-28 15:32:12 +0530

    updated multer image formatting logic

--- controller/userController.js
+++ controller/userController.js
@@ -3,6 +3,7 @@ import User from "../models/user.js";
 import job from "../models/job.js";
 import cloudinary from "cloudinary";
 import { promises as fs } from "fs";
+import { formatImgae } from "../middleware/multer.js";
 
 export const getCurrentUser = async (req, res) => {
   const currentUser = await User.findOne({ _id: req.user.userId })
@@ -23,8 +24,8 @@ export const getApplicationStats = async (req, res) => {
 
 export const updateUser = async (req, res) => {
   if (req.file) {
-    const cloudData = await cloudinary.v2.uploader.upload(req.file.path);
-    fs.unlink(req.file.path);
+    const file = formatImgae(req.file);
+    const cloudData = await cloudinary.v2.uploader.upload(file);
     req.body.avatar = cloudData.secure_url;
     req.body.avatarPublicId = cloudData.public_id;
   }
--- middleware/multer.js
+++ middleware/multer.js
@@ -1,14 +1,16 @@
 import multer from "multer";
+import DataParser from "datauri/parser.js";
+import path from "path";
+import { log } from "console";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/uploads')
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname)
-  }
-})
-
+const storage = multer.memoryStorage();
 const multerUpload = multer({ storage });
+const parser = new DataParser();
+
+export const formatImgae = (file) => {
+  log(file);
+  const fileExtension = path.extname(file.originalname).toString();
+  return parser.format(fileExtension, file.buffer).content;
+}
 
 export default multerUpload;
